4c370a5386856764dff73c4030aac7dd945ee5a0
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-11-15 22:53:49 +0900

    Update main.dart

--- Dtrip/lib/main.dart
+++ Dtrip/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_map_plugin/kakao_map_plugin.dart';
-import 'package:dtrip/screens/main_home.dart';
-import 'package:dtrip/screens/places.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart'; // kakao Map API 연결
+import 'package:dtrip/screens/main_home.dart'; // main_home.dart
+import 'package:dtrip/screens/places.dart'; // Place.dart
+import 'package:firebase_core/firebase_core.dart'; // DB 연결
+import 'firebase_options.dart'; // DB 연결
 
-void main() {
-  AuthRepository.initialize(appKey: '124e4b785cbdd3fc46a37b0abd30547f');
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔥 Firebase 초기화
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Kakao Map
+  AuthRepository.initialize(appKey: '124e4b785cbdd3fc46a37b0abd30547f');
+
   runApp(const MyApp());
 }
 
