03cb38802ca1048109e00c1a355c514dbebd5b42
Minjun Jo <165036607+JoM1njun@users.noreply.github.com>
2025-11-17 00:07:10 +0900

    Add files via upload

---
+++ Dtrip/lib/services/place_data.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/place_model.dart';
+
+class PlaceService {
+  final CollectionReference placesRef = FirebaseFirestore.instance.collection(
+    'places',
+  );
+
+  Future<List<Place>> fetchPopularPlaces() async {
+    final query = await placesRef.where('popular', isEqualTo: true).get();
+
+    return query.docs
+        .map((doc) => Place.fromDocument(doc.data() as Map<String, dynamic>))
+        .toList();
+  }
+}
